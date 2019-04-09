b2d96bf46c588109becc8047f0bb01af2f1ce33f
kraiem12 <krayem1989rim@gmail.com>
2019-04-09 01:22:17 -0700

    supp2

--- script_lancement.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#destination : /home/Desktop/scripts/script_lancement.bash
-
-#Lancement du script d'installation de la base de donnée
-./script_BDD.bash
-
- #Lancement du script d'installation du site wordpress
-./script_wordpress.bash
-
